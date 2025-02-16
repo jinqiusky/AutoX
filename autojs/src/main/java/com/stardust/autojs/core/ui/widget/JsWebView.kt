23dc1d5a340b622a0cb55b9bb06d0d6c446a8848
mis1230 <142562847+mis1230@users.noreply.github.com>
2025-02-16 19:23:54 +0800

    Update JsWebView.kt

--- autojs/src/main/java/com/stardust/autojs/core/ui/widget/JsWebView.kt
+++ autojs/src/main/java/com/stardust/autojs/core/ui/widget/JsWebView.kt
@@ -14,13 +14,20 @@ open class JsWebView : WebView {
 
     init {
         val settings = settings
-        settings.useWideViewPort = true
-        settings.builtInZoomControls = true
-        settings.loadWithOverviewMode = true
-        settings.javaScriptEnabled = true
-        settings.javaScriptCanOpenWindowsAutomatically = true
-        settings.domStorageEnabled = true
-        settings.displayZoomControls = false
+        settings.useWideViewPort = true //启用宽视图端口
+        settings.builtInZoomControls = true  //启用内置缩放控件。
+        settings.loadWithOverviewMode = true //启用概述模式加载页面
+        settings.javaScriptEnabled = true  //启用 JavaScript
+        settings.javaScriptCanOpenWindowsAutomatically = true  //允许 JavaScript 自动打开新窗口
+        settings.domStorageEnabled = true  //启用 DOM 存储（Web 存储）
+        settings.displayZoomControls = false  //隐藏内置缩放控件的显示
+
+        settings.cacheMode = WebSettings.LOAD_NO_CACHE // 禁用缓存
+        settings.allowFileAccess = true        // 允许文件访问
+        settings.allowContentAccess = true        // 允许内容访问
+        settings.allowFileAccessFromFileURLs = true   // 允许从文件URL进行文件访问
+        settings.allowUniversalAccessFromFileURLs = true  // 允许从任何来源进行访问
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) webViewClient = JsBridge.SuperWebViewClient()
     }
 
